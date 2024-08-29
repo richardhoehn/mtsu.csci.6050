fbc62e7221d4d6b80a2faab7a3f7b7dfca8e32bc
Richard Hoehn <rhoehn@gmail.com>
2024-08-29 12:41:30 -0500

    Starting Project 1

---
+++ project01/project1.c
@@ -0,0 +1,20 @@
+/*
+
+Name:  Richard Hoehn
+Class: CSCI-6050
+Proj:  01
+Date:  2024-09-09
+Desc:  Hex Creater App
+
+*/
+
+// Setup Includes & Libs
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Start Program
+int main(int argc, char* argv[]){
+
+    return 0;
+}
---
+++ project01/test_project1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gcc ./project1.c -o project1
+
+./project1 test.txt
+echo ""
+echo ""
+
+./project1 project.c
+echo ""
+echo ""
