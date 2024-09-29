aa397dec8361673125602ff7d8784a1cf803ddd9
Richard Hoehn <rhoehn@gmail.com>
2024-09-29 16:40:46 -0500

    Lab 6 Updates

--- lab06/lab6Problem1.c
+++ lab06/lab6Problem1.c
@@ -15,6 +15,39 @@ typedef struct {
 
 } Struct_Employee_Info;
 
+
+// Creating Definition Functions for the Operators!
+int greater_than(int a, int b) { return a > b; }
+int less_than(int a, int b) { return a < b; }
+int greater_than_or_equal(int a, int b) { return a >= b; }
+int less_than_or_equal(int a, int b) { return a <= b; }
+int equal(int a, int b) { return a == b; }
+
+// With this special Funciotn we are mapping based on the comparison operator string
+// Note: This is a pointer coming back from the function to the definition
+int (*get_comp_func(const char *operator))(int, int) {
+    if (strcmp(operator, ">") == 0)  return &greater_than;
+    if (strcmp(operator, "<") == 0)  return &less_than;
+    if (strcmp(operator, ">=") == 0) return &greater_than_or_equal;
+    if (strcmp(operator, "<=") == 0) return &less_than_or_equal;
+    if (strcmp(operator, "==") == 0) return &equal;
+    return NULL;  // Return NULL if operator is invalid
+}
+
+// Function to append employee info into the result buffer using snprintf
+void append_employee_info(char *result, Struct_Employee_Info e) {
+    snprintf(
+        result + strlen(result), // Set the location to start writting
+        MAX_SIZE - strlen(result), // Max Length to stop
+        "Name: %s %s\tZip code: %s\tDepartment: %s\tSalary: %d\n", // Formatted String
+        e.firstName, 
+        e.lastName, 
+        e.zipCode,
+        e.department,
+        e.salary);
+}
+
+
 char* searchByName(Struct_Employee_Info employees[], int count, const char *firstName, const char *lastName) {
     //creating a character array to store the result of the search operation
     char *result = malloc(MAX_SIZE * sizeof(char));
@@ -22,6 +55,29 @@ char* searchByName(Struct_Employee_Info employees[], int count, const char *firs
      result, by writing zeros (bytes containing '\0') to that area.*/
     bzero(result,MAX_SIZE);
     //TODO Start:
+
+    // Flag "found"
+    int found = 0;
+
+    // Start For-Loop
+    for(int i=0; i < count; i++){
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Struct_Employee_Info e = employees[i];
+
+        // Check First Name & Last Name
+        // I am using a simple "or" and then to see if the value is still "zero"!
+        if((strcmp(e.firstName, firstName) | strcmp(e.lastName, lastName)) == 0){
+            // Set "found" flag to true
+            found = 1;
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if(found == 0){
+        strcpy(result, "No matching records found.\n");
+    }
 
     //TODo End
     return result;
@@ -35,6 +91,29 @@ char* searchByZipCode(Struct_Employee_Info employees[], int count, const char *z
     bzero(result,MAX_SIZE);
     //TODO Start:
 
+    // Flag "found"
+    int found = 0;
+
+    // Start For-Loop
+    for(int i=0; i < count; i++){
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Struct_Employee_Info e = employees[i];
+
+        // Check Zip Code are equal to eachother
+        if(strcmp(e.zipCode, zipCode) == 0){
+            // Set "found" flag to true
+            found = 1;
+
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if(found == 0){
+        strcpy(result, "No matching records found.\n");
+    }
+
     //TODo End
     return result;
 }
@@ -46,6 +125,31 @@ char* searchBySalary(Struct_Employee_Info employees[], int count, int salary, co
      result, by writing zeros (bytes containing '\0') to that area.*/
     bzero(result,MAX_SIZE);
     //TODO Start:
+
+    // Flag "found"
+    int found = 0;
+
+    // Get the function pointer based on the operator string
+    int (*comparison)(int, int) = get_comp_func(comparisonOperator);
+
+     // Start For-Loop
+    for(int i=0; i < count; i++){
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Struct_Employee_Info e = employees[i];
+
+        if(comparison(e.salary, salary)){
+            // Set "found" flag to true
+            found = 1;
+            
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if(found == 0){
+        strcpy(result, "No matching records found.\n");
+    }
 
     //TODo End
     return result;
---
+++ lab06/lab6Problem1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc ./lab6Problem1.c -o lab6Problem1
+
+./lab6Problem1
