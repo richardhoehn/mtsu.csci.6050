781068bd594f19d70907a39e3931e3eb6652f4c7
Richard Hoehn <rhoehn@gmail.com>
2024-11-16 08:29:35 -0600

    Start on P4

---
+++ project04/client.c
@@ -0,0 +1,390 @@
+/*
+
+Name:  Richard Hoehn
+Class: CSCI-6050
+Proj:  03
+Date:  2024-11-07
+Desc:  Client App
+
+*/
+
+// Libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Includes
+#include "csapp.h"
+
+// Function Declarations
+int isNumber(const char *str);
+void printStars(int);
+void printError(const char *);
+int promptMenuSelect(void);
+char *promptLine(char *);
+int promptLineInt(char *);
+char *promptLineStr(char *);
+int promptLineOps(char *);
+void sendPayload(int, char *);
+
+// Constants
+const int MAX_INPUT_LENGTH = 100;
+const char *VALID_OPS[] = {">", "<", "==", ">=", "<="}; // Allowed operators
+const int VALID_OPS_NUM = 5;                            // Number of allowed operators
+
+/* -----------------------------------------------------------------------
+Func: Main App Loop
+----------------------------------------------------------------------- */
+int main(int argc, char *argv[])
+{
+    // Set Variables
+    int clientFd;      // File Descriptor to communicate with the server
+    char *host, *port; // String of Port & Host
+    size_t n;
+
+    char buffer[MAXLINE]; // MAXLINE = 8192 defined in csapp.h
+    char payload[MAXLINE];
+
+    // Make suer we got the correct argument count
+    if (argc != 3)
+    {
+        printError("Missing Correct Count of Arguments!");
+        printError("App needs: 'host' & 'port' entered as arguments");
+        exit(0);
+    }
+
+    // Set host & Port for Connections
+    host = argv[1];
+    port = argv[2];
+
+    // Validate port as a number
+    if (!isNumber(port)) {
+        printError("Port must be a valid number.");
+        exit(1);
+    }
+
+    clientFd = Open_clientfd(host, port); // wrapper function that calls getadderinfo, socket and connect functions for client side
+
+    // Resetting the Buffer!
+    bzero(buffer, MAXLINE);
+
+    while (1)
+    {
+        // Start Prompt
+        int sel = promptMenuSelect();
+
+        // Spacer
+        printf("\n");
+
+        // Setup Vars
+        char *firstName = NULL;
+        char *lastName = NULL;
+        char *department = NULL;
+        char *zipCode = NULL;
+        int salary;
+        int opIdx;
+
+        // Add Record
+        if (sel == 1)
+        {
+            firstName = promptLineStr("Enter First Name:");
+            lastName = promptLineStr("Enter Last Name:");
+            zipCode = promptLineStr("Enter Zip Code:");
+            department = promptLineStr("Enter Department:");
+            salary = promptLineInt("Enter Salary:");
+            sprintf(payload, "1,%s,%s,%s,%s,%d", firstName, lastName, zipCode, department, salary);
+            sendPayload(clientFd, payload);
+        }
+
+        // Search By Name
+        if (sel == 2)
+        {
+            firstName = promptLineStr("Enter First Name:");
+            lastName = promptLineStr("Enter Last Name:");
+            sprintf(payload, "2,%s,%s", firstName, lastName);
+            sendPayload(clientFd, payload);
+        }
+
+        // Search by Zip Code
+        if (sel == 3)
+        {
+            zipCode = promptLineStr("Enter Zip Code:");
+            sprintf(payload, "3,%s", zipCode);
+            sendPayload(clientFd, payload);
+        }
+
+        // Search by Salary
+        if (sel == 4)
+        {
+            salary = promptLineInt("Enter Salary:");
+            printf("Salary: %d\n", salary);
+            opIdx = promptLineOps("Enter Comparision Type ['>','<','==','>=','<=']:");
+            sprintf(payload, "4,%d,%s", salary, VALID_OPS[opIdx]);
+            sendPayload(clientFd, payload);
+        }
+
+        // Free Vars - Potential Memory Leak
+
+        free(firstName);
+        free(lastName);
+        free(department);
+        free(zipCode);
+
+        // Terminate
+        if (sel == 5)
+        {
+            sprintf(payload, "5");
+            printf("Payload: %s\n", payload);
+            sendPayload(clientFd, payload);
+            break;
+        }
+    }
+
+    // Close Server Connection
+    Close(clientFd);
+
+    return 0;
+}
+
+
+void sendPayload(int fd, char * payload)
+{
+    char buffer[MAXLINE]; // MAXLINE = 8192 defined in csapp.h
+    memset(buffer, 0, sizeof(buffer)); // Clear the buffer by setting all bytes to '\0'
+
+    // sending the message received from the user to the server
+    int n = write(fd, payload, strlen(payload));
+    if (n < 0)
+    {
+        perror("Write Error!!");
+        return;
+    }
+
+    // waiting for the message from the server.
+    // the message will be stored in buffer variable.
+    n = read(fd, buffer, MAXLINE);
+    if (n < 0)
+    {
+        perror("Read Error!!");
+        return;
+    }
+
+    printf("\nMessage from Server:\n");
+    // displaying the message in buffer on the console
+    Fputs(buffer, stdout);
+}
+
+
+/* -----------------------------------------------------------------------
+Func: isNumber()
+What: This function check if the string is a number
+In:   String
+Out:  Int
+----------------------------------------------------------------------- */
+int isNumber(const char *str) {
+    // Check if each character in the string is a digit
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isdigit((unsigned char)str[i])) {
+            return 0;  // Return 0 if any character is not a digit
+        }
+    }
+    return 1;  // Return 1 if all characters are digits
+}
+
+/* -----------------------------------------------------------------------
+Func: promptLineInt()
+What: This function prints a line and asks for input of Int
+In:   String
+Out:  Int
+----------------------------------------------------------------------- */
+int promptLineInt(char *prompt)
+{
+    char *out = promptLine(prompt); // Allocate memory for user input
+
+    // Check if the input string is a valid integer
+    int is_valid = isNumber(out);
+
+    if (is_valid)
+    {
+        int result = atoi(out); // Convert string to integer
+        free(out);              // Free memory before returning
+        return result;
+    }
+    else
+    {
+        printError("Please enter a valid integer.");
+        free(out);                    // Free memory before retrying
+        return promptLineInt(prompt); // Repeat the prompt
+    }
+}
+
+/* -----------------------------------------------------------------------
+Func: promptLineStr()
+What: This function prints a line and asks for non-empty string input
+In:   String (prompt)
+Out:  String (non-empty input)
+----------------------------------------------------------------------- */
+char *promptLineStr(char *prompt)
+{
+    char *out = promptLine(prompt); // Allocate memory for user input
+
+    // Check if the input string is non-empty
+    if (out != NULL && strlen(out) > 0)
+    {
+        return out; // Return the valid, non-empty string
+    }
+    else
+    {
+        printError("Input cannot be empty. Please enter at least one character.");
+        free(out);                    // Free memory before retrying
+        return promptLineStr(prompt); // Repeat the prompt
+    }
+}
+
+/* -----------------------------------------------------------------------
+Func: promptLineOps()
+What: This function prompts the user to input a comparison operator
+      from the allowed options: ['>','<','==','>=','<=']
+In:   String (prompt)
+Out:  Int (index of valid operator in the array)
+----------------------------------------------------------------------- */
+int promptLineOps(char *prompt)
+{
+    char *out = NULL;
+    int index = -1;
+
+    while (index == -1)
+    {
+        out = promptLine(prompt); // Get user input
+
+        // Check if the input matches any of the valid operators
+        for (int i = 0; i < VALID_OPS_NUM; i++)
+        {
+            if (strcmp(out, VALID_OPS[i]) == 0)
+            {
+                index = i; // Store the index of the matched operator
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            printError("Invalid operator.");
+            free(out); // Free memory before retrying
+        }
+    }
+
+    free(out);    // Free memory after a valid input is found
+    return index; // Return the index of the valid operator
+}
+
+/* -----------------------------------------------------------------------
+Func: promptLine()
+What: This function prints a line and asks for input then returns
+In:   String
+Out:  String
+----------------------------------------------------------------------- */
+char *promptLine(char *prompt)
+{
+    char *resp = malloc(MAX_INPUT_LENGTH); // Allocate memory for the response
+    if (resp == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+
+    printf("%s ", prompt);
+
+    // Read input and store it directly in `resp`
+    if (fgets(resp, MAX_INPUT_LENGTH, stdin) != NULL)
+    {
+        // Remove newline if present
+        resp[strcspn(resp, "\n")] = '\0';
+        return resp; // Return the input as a string
+    }
+    else
+    {
+        printf("Error reading input.\n");
+        free(resp);
+        return NULL;
+    }
+}
+
+/* -----------------------------------------------------------------------
+Func: printStars()
+What: This function prints a line of "stars" ending with a new-line
+In:   int - Stars to print
+Out:  void
+----------------------------------------------------------------------- */
+void printStars(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+
+/* -----------------------------------------------------------------------
+Func: printError()
+What: This function prints an error message based on input
+In:   char array
+Out:  void
+----------------------------------------------------------------------- */
+void printError(const char *strError)
+{
+    printf("\n");
+    printStars(strlen(strError) + 8);
+    printf("!!! %s !!!\n", strError);
+    printStars(strlen(strError) + 8);
+    printf("\n");
+}
+
+/* -----------------------------------------------------------------------
+Func: promptMenuSelect()
+What: This function asks use to select an option
+In:   void
+Out:  int (1-5)
+----------------------------------------------------------------------- */
+int promptMenuSelect(void)
+{
+    int resp = 0;
+
+    while (1)
+    {
+        printf("\n");
+        printf("(1) Add record\n");
+        printf("(2) Search by Name\n");
+        printf("(3) Search by Zip Code\n");
+        printf("(4) Search by Salary\n");
+        printf("(5) Terminate\n");
+        printf("Select an option [1,2,3,4 or 5]: ");
+
+        // Make sure Input is a Number!
+        if (scanf("%d", &resp) != 1)
+        {
+            printError("Invalid input! Please enter a number.");
+
+            // Clear the Buffer!
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+
+        // Clear any additional leftover characters in the buffer
+        while (getchar() != '\n')
+            ;
+
+        if (resp >= 1 && resp <= 5)
+        {                // Check if input is within valid range
+            return resp; // Return valid input
+        }
+        else
+        {
+            printError("Wrong code! Please try again.");
+        }
+    }
+
+    return resp;
+}
---
+++ project04/server.c
@@ -0,0 +1,391 @@
+/*
+
+Name:  Richard Hoehn
+Class: CSCI-6050
+Proj:  03
+Date:  2024-11-07
+Desc:  Server App
+
+*/
+
+// Libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Includes
+#include "csapp.h"
+
+#define MAX_SIZE 1000
+#define MAX_LENGTH 25
+#define FILENAME "records.csv"
+
+typedef struct
+{
+    char firstName[MAX_LENGTH];
+    char lastName[MAX_LENGTH];
+    char zipCode[MAX_LENGTH];
+    char department[MAX_LENGTH];
+    int salary;
+
+} Employee;
+
+// Function Declarations
+char *serverRead(int);
+void serverWrite(int, char *);
+void printStars(int);
+void printError(const char *);
+int updateEmployees(Employee employees[]);
+void appendToFile(char *);
+
+// Creating Definition Functions for the Operators!
+int greater_than(int a, int b) { return a > b; }
+int less_than(int a, int b) { return a < b; }
+int greater_than_or_equal(int a, int b) { return a >= b; }
+int less_than_or_equal(int a, int b) { return a <= b; }
+int equal(int a, int b) { return a == b; }
+
+// With this special Funciotn we are mapping based on the comparison operator string
+// Note: This is a pointer coming back from the function to the definition
+int (*get_comp_func(const char *operator))(int, int) {
+    if (strcmp(operator, ">") == 0) { return &greater_than; }
+    if (strcmp(operator, "<") == 0) { return &less_than; }
+    if (strcmp(operator, ">=") == 0) { return &greater_than_or_equal; }
+    if (strcmp(operator, "<=") == 0) { return &less_than_or_equal; }
+    if (strcmp(operator, "==") == 0) { return &equal; }
+    return NULL; // Return NULL if operator is invalid
+}
+
+// Function to append employee info into the result buffer using snprintf
+void append_employee_info(char *result, Employee e) {
+    snprintf(
+        result + strlen(result),   // Set the location to start writting
+        MAX_SIZE - strlen(result), // Max Length to stop
+        "%s,%s,%s,%s,%d\n",        // Formatted String
+        e.firstName,
+        e.lastName,
+        e.zipCode,
+        e.department,
+        e.salary);
+}
+
+char *searchByName(Employee employees[], int count, const char *firstName, const char *lastName) {
+    char *result = malloc(MAX_SIZE * sizeof(char));
+    bzero(result, MAX_SIZE);
+
+    // Flag "found"
+    int found = 0;
+
+    // Start For-Loop
+    for (int i = 0; i < count; i++) {
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Employee e = employees[i];
+
+        // Check First Name & Last Name
+        // I am using a simple "or" and then to see if the value is still "zero"!
+        if ((strcmp(e.firstName, firstName) | strcmp(e.lastName, lastName)) == 0) {
+            // Set "found" flag to true
+            found = 1;
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if (found == 0) {
+        strcpy(result, "No matching records found.\n");
+    }
+
+    return result;
+}
+
+char *searchByZipCode(Employee employees[], int count, const char *zipCode) {
+    char *result = malloc(MAX_SIZE * sizeof(char));
+    bzero(result, MAX_SIZE);
+
+    // Flag "found"
+    int found = 0;
+
+    // Start For-Loop
+    for (int i = 0; i < count; i++) {
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Employee e = employees[i];
+
+        // Check Zip Code are equal to eachother
+        if (strcmp(e.zipCode, zipCode) == 0) {
+            // Set "found" flag to true
+            found = 1;
+
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if (found == 0) {
+        strcpy(result, "No matching records found.\n");
+    }
+
+    return result;
+}
+
+char *searchBySalary(Employee employees[], int count, int salary, const char *comparisonOperator) {
+    char *result = malloc(MAX_SIZE * sizeof(char));
+    bzero(result, MAX_SIZE);
+
+    // Flag "found"
+    int found = 0;
+
+    // Get the function pointer based on the operator string
+    int (*comparison)(int, int) = get_comp_func(comparisonOperator);
+
+    // Start For-Loop
+    for (int i = 0; i < count; i++) {
+        // Create a copy of the Empoyee - Makes Code a bit easier
+        Employee e = employees[i];
+
+        // Based on the Comparrison we will get a "0" or "1"
+        if (comparison(e.salary, salary))
+        {
+            // Set "found" flag to true
+            found = 1;
+
+            // Append Employee to results
+            append_employee_info(result, e);
+        }
+    }
+
+    // Check found Flag
+    if (found == 0)
+    {
+        strcpy(result, "No matching records found.\n");
+    }
+
+    return result;
+}
+
+// Main Server Application!
+int main(int argc, char *argv[])
+{
+    int listenfd;
+    int connFd; // file descriptor to communicate with the client
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr; /* Enough space for any address */
+
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+
+    listenfd = Open_listenfd(argv[1]); // wrapper function that calls getadderinfo, socket, bind, and listen functions in the server side
+
+    // Server runs in the infinite loop.
+    // To stop the server process, it needs to be killed using the Ctrl+C key.
+    while (1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+
+        // wait for the connection from the client.
+        connFd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname,
+                    MAXLINE, client_port, MAXLINE, 0);
+
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+
+         // Employee Setup
+        Employee employees[MAX_SIZE];
+        int employeeCount = updateEmployees(employees);
+
+        while (1)
+        {
+            // Read the payload from client
+            char *buffer = serverRead(connFd);
+            if (buffer == NULL)
+            {
+                printError("Buffer Corrupt!");
+                break;
+            }
+
+            // Parse the buffer in the main loop
+            char *token = strtok(buffer, ",");
+            int sel = atoi(token); // Convert the first token to an int
+
+            if(sel == 1){
+                char *record = strtok(NULL, "\0");  // Capture the rest of the buffer as the record
+                appendToFile(record);
+                employeeCount = updateEmployees(employees);
+                serverWrite(connFd, "Record added Sucessfully!!\n");
+            }
+
+            if(sel == 2){
+                char *firstName = strtok(NULL, ",");  // Get First Name
+                char *lastName = strtok(NULL, ",");  // Get Last Name
+                char *searchResult = searchByName(employees, employeeCount, firstName, lastName);
+                serverWrite(connFd, searchResult);
+            }
+
+            if(sel == 3){
+                char *zipCode = strtok(NULL, ",");  // Get Zip Code
+                char *searchResult = searchByZipCode(employees, employeeCount, zipCode);
+                serverWrite(connFd, searchResult);
+            }
+
+            if(sel == 4){
+                int salary = atoi(strtok(NULL, ","));
+                char *op = strtok(NULL, ",");
+                char *searchResult = searchBySalary(employees, employeeCount, salary, op);
+                serverWrite(connFd, searchResult);
+            }
+                
+            if (sel == 5)
+            {
+                serverWrite(connFd, "Connection Closed!!\n");
+                break; // Exit Reading Loop & close Connection
+            }
+            
+
+            free(buffer); // Free allocated buffer after processing
+        }
+
+        Close(connFd);
+        printf("(%s, %s) disconnected\n", client_hostname, client_port);
+    }
+    exit(0);
+}
+
+
+/* -----------------------------------------------------------------------
+Func: updateEmployees()
+What: This function prints an error message based on input
+In:   - employee Array - Pointer
+Out:  - (int) count of employees
+----------------------------------------------------------------------- */
+int updateEmployees(Employee employees[]) {
+    FILE *file = fopen(FILENAME, "r");
+    int count = 0;
+
+    if (file == NULL) {
+        printf("Unable to open the file.\n");
+        return 1;
+    }
+
+    char line[MAX_SIZE];
+
+    while (fgets(line, sizeof(line), file)) {
+        sscanf(line, "%[^,],%[^,],%[^,],%[^,],%d",
+               employees[count].firstName,
+               employees[count].lastName,
+               employees[count].zipCode,
+               employees[count].department,
+               &employees[count].salary);
+        count++;
+    }
+
+    fclose(file); // Close File!
+    return count; // Return employee count!
+}
+
+
+void appendToFile(char *record) {
+    FILE *file = fopen(FILENAME, "a");  // Open in append mode
+    if (file == NULL) {
+        printf("Unable to open the file.\n");
+        return;
+    }
+
+    // Write the record and add a newline
+    fprintf(file, "\n%s", record);
+
+    fclose(file); // Close the file
+    return;
+}
+
+
+
+/* -----------------------------------------------------------------------
+Func: printError()
+What: This function prints an error message based on input
+In:   (char *) Text to print
+Out:  void
+----------------------------------------------------------------------- */
+void printError(const char *strError)
+{
+    printf("\n");
+    printStars(strlen(strError) + 8);
+    printf("!!! %s !!!\n", strError);
+    printStars(strlen(strError) + 8);
+    printf("\n");
+}
+
+
+/* -----------------------------------------------------------------------
+Func: serverRead()
+What: This function read data from client
+In:   (int) File Descriptor
+Out:  (char *) - Payload sent
+----------------------------------------------------------------------- */
+char *serverRead(int fd)
+{
+    char *buffer = malloc(MAXLINE); // Allocate buffer on the heap
+    if (buffer == NULL)
+    {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+
+    bzero(buffer, MAXLINE);
+
+    // Read data from the client
+    size_t n = read(fd, buffer, MAXLINE - 1); // Leave space for null terminator
+    if (n < 0)
+    {
+        perror("Read Error!!");
+        free(buffer); // Free the allocated memory on error
+        return NULL;
+    }
+
+    buffer[n] = '\0'; // Null-terminate the string
+
+    return buffer; // Return the buffer directly
+}
+
+
+/* -----------------------------------------------------------------------
+Func: serverWrite()
+What: This function writes back to Client
+In: - (int) File Descriptor
+    - (char *) Payload to send 
+Out:  void
+----------------------------------------------------------------------- */
+void serverWrite(int fd, char * payload)
+{
+    int n = write(fd, payload, strlen(payload));
+    if (n < 0)
+    {
+        perror("Write Error!!");
+        return;
+    }
+}
+
+
+/* -----------------------------------------------------------------------
+Func: printStars()
+What: This function prints a line of "stars" ending with a new-line
+In:   int - Stars to print
+Out:  void
+----------------------------------------------------------------------- */
+void printStars(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+
+
